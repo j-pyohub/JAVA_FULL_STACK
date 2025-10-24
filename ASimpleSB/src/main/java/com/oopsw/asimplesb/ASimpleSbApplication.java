157832ecafdb9e481f99cd1cafb79a4e2c578f78
j-pyohub <pyosuejung@gmail.com>
2025-10-24 17:55:24 +0900

    10/24 springboot start

---
+++ ASimpleSB/src/main/java/com/oopsw/asimplesb/ASimpleSbApplication.java
@@ -0,0 +1,13 @@
+package com.oopsw.asimplesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASimpleSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ASimpleSbApplication.class, args);
+    }
+
+}
---
+++ ASimpleSB/src/test/java/com/oopsw/asimplesb/ASimpleSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopsw.asimplesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASimpleSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
