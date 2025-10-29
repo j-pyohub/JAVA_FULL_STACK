a0911b1394f2913c167e8c22402ab9cb4341670b
j-pyohub <pyosuejung@gmail.com>
2025-10-29 18:41:10 +0900

    10/29 refactor

--- BController/src/main/java/com/oopsw/bcontroller/controller/CustomerRestController.java
+++ BController/src/main/java/com/oopsw/bcontroller/controller/CustomerRestController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor //필수. 상속, 생성자 --> final로 메모리에 한번만 올라가게 함. 대신 interface 하나당 class 한개여야 함
+@RequiredArgsConstructor //필수. 상속, 생성자 --> final로 메모리에 한번만 올라가게 함.
 public class CustomerRestController {
-    private final CustomerService customerService; //final: 초기화 1번만. interface와 관련된 class 알아서 찾아감 -> 동적으로 구현부 바꿈
+    private final CustomerService customerService; //final: 초기화 1번만. interface와 관련된 class 알아서 찾아감. 대신 interface 하나당 class 한개여야 함-> 동적으로 구현부 바꿈
     //특정 고객의 정보 확인 - api/customer/pyo(선택) or api/customer?customer_id=pyo
     //view or JSON(선택)
     @GetMapping("api/customer/{customerId}") //요구사항에 맞추어 입력, 출력, url 결정
@@ -21,8 +21,10 @@ public class CustomerRestController {
         //service
         //CustomerService customerService = new CustomerServiceA(); //spring을 쓰는 이점X - 계속 생성됨
         CustomerVO customerVO = customerService.getCustomer(customerId);
-        CustomerResponse response = CustomerResponse.builder().customerId(customerVO.getCustomerId())
-                                    .name(customerVO.getName()).point(customerVO.getPoint()).build();
+        CustomerResponse response = CustomerResponse.builder()
+                                    .customerId(customerVO.getCustomerId())
+                                    .name(customerVO.getName())
+                                    .point(customerVO.getPoint()).build();
 
         return ResponseEntity.status(201).body(response); //상황에 맞추어 status 번호 적기
     }
