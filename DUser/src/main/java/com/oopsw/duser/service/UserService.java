0b491ae4e67ca9945dcd12cead08b0e819c9230f
j-pyohub <pyosuejung@gmail.com>
2025-11-12 19:06:14 +0900

    11/12 test

--- DUser/src/main/java/com/oopsw/duser/service/UserService.java
+++ DUser/src/main/java/com/oopsw/duser/service/UserService.java
@@ -1,12 +1,17 @@
 package com.oopsw.duser.service;
 
+import com.oopsw.duser.repository.User;
+import com.oopsw.duser.repository.UserRepository;
 import com.oopsw.duser.vo.UserVO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
 
     @Autowired //메모리에 올린 거 가져옴
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -14,14 +19,20 @@ public class UserService {
     //회원가입
     public boolean join(UserVO userVO){
         //입력받은 데이터 중복 여부 확인
-
+        if (userRepository.existsByEmail(userVO.getEmail())){
+            return false;
+        }
+        if (userRepository.existsByUsername(userVO.getUsername())){
+            return false;
+        }
         //UserRequest -> UserVO (비밀번호, Role)
         userVO.setPassword(bCryptPasswordEncoder.encode(userVO.getPassword()));
         userVO.setRole("ROLE_USER");
         //userVO.setRole("ROLE_ADMIN");
         //userVO.setRole("ROLE_MANAGER");
-        System.out.println(userVO);
+        //System.out.println(userVO);
         //테이블에 등록 후 true/false 결정
+        userRepository.save(User.builder().username(userVO.getUsername()).password(userVO.getPassword()).email(userVO.getEmail()).role(userVO.getRole()).build());
         return true;
     }
 }
