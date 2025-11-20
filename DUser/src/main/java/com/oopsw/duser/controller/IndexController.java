dc23cf63764e0d7f7413e3a7822f469e6f46f65a
j-pyohub <pyosuejung@gmail.com>
2025-11-20 14:45:55 +0900

    test

--- DUser/src/main/java/com/oopsw/duser/controller/IndexController.java
+++ DUser/src/main/java/com/oopsw/duser/controller/IndexController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.security.Principal;
-
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
