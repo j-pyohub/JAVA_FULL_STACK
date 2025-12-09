30a35ff36b1c75c2ed32f3bacadc46ff30892374
j-pyohub <pyosuejung@gmail.com>
2025-12-09 18:13:51 +0900

    12/9 JWT

---
+++ EJWT/src/test/java/com/oopsw/ejwt/EjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oopsw.ejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
